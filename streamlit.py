51653e5345fae89b5fcc5dd292f559e8c3b22502
DBATOK <112704612+DBATOK@users.noreply.github.com>
2023-03-14 17:15:24 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.title('Hello, Streamlit!')
+headerSection = st.container
+mainSection = st.container
+LeftNav = st.sidebar
+with headerSection:
+  st.title("Stremlit")
+  st.markdown("Blabla")
+with LeftNav:
+  st.title("LV")
+  st.markdown("DD")
